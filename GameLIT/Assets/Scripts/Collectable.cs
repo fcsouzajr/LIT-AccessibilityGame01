78a6005de3d309fb8cc130da55938875a7612385
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2021-08-26 21:16:58 -0300

    Movimentação do personagem e programação de coletáveis

---
+++ GameLIT/Assets/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    public GameObject image;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+            image.active = true;
+        }
+    }
+}
---
+++ GameLIT/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    Rigidbody2D body;
+    SpriteRenderer sprite;
+    float moveX = 0;
+    bool jumpPressed = false;
+    public float speed;
+    public float jumpForce;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update() {
+        moveX = Input.GetAxis("Horizontal");
+        jumpPressed = Input.GetButtonDown("Jump");
+
+        if (moveX != 0)
+            Walk();
+
+        if (jumpPressed)
+            Jump();
+
+        if (body.velocity.x > 0 && sprite.flipX ||
+            body.velocity.x < 0 && !sprite.flipX)
+            Flip();
+    }
+
+    void Flip() {
+        sprite.flipX = !sprite.flipX;
+    }
+
+    void Walk() {
+        body.velocity = new Vector2(moveX * speed, body.velocity.y);
+    }
+
+    void Jump() {
+        body.AddForce(new Vector2(0, jumpForce));
+    }
+}
