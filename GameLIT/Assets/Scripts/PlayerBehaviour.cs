dd159beb5f08205577b43c929a0ff5a52e4e046e
JVJVJVJVJVJV <pinheiro.v@escolar.ifrn.edu.br>
2021-11-04 18:26:43 -0300

    Ataque do player implementado
    Co-Authored-By: Guilherme Medeiros <50681750+GuiMedeirox@users.noreply.github.com>

--- GameLIT/Assets/Scripts/PlayerBehaviour.cs
+++ GameLIT/Assets/Scripts/PlayerBehaviour.cs
@@ -22,15 +22,23 @@ public class PlayerBehaviour : MonoBehaviour {
     public float speed;
     float moveX = 0;
 
+    [Header("Attack Variables")]
+    public Transform enemyCheck;
+    public float enemyCheckRadius;
+    public LayerMask whatIsEnemy;
+
     void Start() {
         body = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
     }
 
-    void FixedUpdate() {
+    void Update() {
         moveX = Input.GetAxis("Horizontal");
         isOnFloor = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetButtonDown("Fire1") && isOnFloor)
+            anim.SetTrigger("attack");
 
         if (body.velocity.y < 0)
             body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier-1) * Time.deltaTime;
@@ -50,6 +58,7 @@ public class PlayerBehaviour : MonoBehaviour {
 
     void Flip() {
         sprite.flipX = !sprite.flipX;
+        enemyCheck.localPosition = new Vector2(-enemyCheck.localPosition.x, enemyCheck.localPosition.y);
     }
 
     void Walk() {
@@ -65,9 +74,13 @@ public class PlayerBehaviour : MonoBehaviour {
         anim.SetFloat("speedY", body.velocity.y);
     }
 
+    void Attack() {
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(enemyCheck.position, enemyCheckRadius);
     }
 }
