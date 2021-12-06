21543204e1fd250828267b607850948d6743f306
Denner Bismarck <67791717+DennerBismarck@users.noreply.github.com>
2021-12-06 10:14:50 -0300

    MenuInicial
    Com funcionamento básico de teste

---
+++ GameLIT/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public string nomedacena;
+
+    public void changeS() {
+        SceneManager.LoadScene(nomedacena);
+    }
+
+    public void sair()
+    {
+        Application.Quit();
+    }
+
+}
