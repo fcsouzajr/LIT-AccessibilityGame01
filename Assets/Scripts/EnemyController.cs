3ae78360bcbb8afce952f2f6faf6c6cf428623de
Paulinho <paulo.tarso@escolar.ifrn.edu.br>
2022-06-30 16:51:34 -0300

    Criando Controller dos Inimigos
    Primeiras variáveis do controlador dos inimigos.
    Cada vez que um for criado, usará este script para receber valores de vida, etc, e tudo que um inimigo precisará;
    ======
    Pra que cada inimigo gerado não carregue com si, as mesmas variáveis copiadas

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+  //[Header("Components")]
+  Rigidbody2D body;
+  SpriteRenderer sprite;
+  Animator anim;
+
+  //[Header("Attack Variables")]
+  public float attackDamage;
+  public float delayAttackValue;
+  private float delayAttack;
+  private bool isDead = false;
+  public float hp = 30f;
+  GameObject player;
+
+  //[Header("Move Variables")]
+  public float speed;
+  public float walkTime;
+  private float timer;
+  private bool walkRight;
+}
