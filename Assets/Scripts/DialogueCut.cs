536d22325c7632f076a0870726883c3741c48e51
Isadora Lucena <66965789+Isadoralucena@users.noreply.github.com>
2022-08-11 07:53:20 -0300

    changes

---
+++ Assets/Scripts/DialogueCut.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCut : MonoBehaviour {
+
+    public string speechTxt;
+    public string actorName;
+
+    public LayerMask playerLayer;
+    public float radious;
+
+    private DialogueControl dc;
+	
+	private void Start()
+    {
+        dc = FindObjectOfType<DialogueControl>();
+    }
+    public void Interact()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+        if(hit != null)
+        {
+            dc.Speech(speechTxt, actorName);
+        }
+    }
+}
