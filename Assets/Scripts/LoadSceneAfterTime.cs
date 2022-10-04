2c114fef504527fef9db9921fb1c9a6296b2ef09
Isadoralucena <isadorabeatriz595@gmail.com>
2022-10-04 11:51:29 -0300

    Transições entre as cutscenes

---
+++ Assets/Scripts/LoadSceneAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneAfterTime : MonoBehaviour {
+	[SerializeField]
+	private float delayBeforeLoading = 10f;
+
+	[SerializeField]
+	private string sceneNameToLoad;
+	private float timeElapsed;
+
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		if(timeElapsed > delayBeforeLoading)
+		{
+			SceneManager.LoadScene(sceneNameToLoad);
+		}
+	}
+}
