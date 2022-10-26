2a161f581acfd7b13109e42104704c58bc00f585
Paulinho <paulo.tarso@escolar.ifrn.edu.br>
2022-10-26 02:08:47 -0300

    Animações do Esqueleto e início do script

---
+++ Assets/Scripts/Skeleton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skeleton : MonoBehaviour
+{
+	public Transform rayCast;
+	public LayerMask rayCastMask;
+	public float rayCastLength;
+	public float attackDistance;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
