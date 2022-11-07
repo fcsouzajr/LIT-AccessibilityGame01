e9b79744a38922b766d7bd08623d4ade64ca42ca
Isadora Lucena <66965789+Isadoralucena@users.noreply.github.com>
2022-11-06 21:58:55 -0300

    Scripts da estalactite
    - StalactiteFall usa trigger
    - ManyStalactitesFall usa trigger
    - StalactiteFallTimer usa segundos para a queda
    - problema nos dois colliders da estalactite (circle collider desativado)

---
+++ Assets/Scripts/ManyStalactitesFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManyStalactitesFall : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player")
+		{
+
+			StartCoroutine(fall());
+		}
+	}
+
+	IEnumerator fall()
+	{
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			transform.GetChild(i).gameObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+			yield return new WaitForSeconds(0.1f);;
+		}
+	}
+}
---
+++ Assets/Scripts/StalactiteFallTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteFallTimer : MonoBehaviour {
+
+	public float fallSpeed = 4.0f;
+	[SerializeField]private bool CanFall;
+	public float TimeBeforeFall = 5;
+	
+	void Start()
+	{
+		StartCoroutine(ReadyToFall());
+	}
+
+	void Update()
+	{
+		if(CanFall == true)
+		{
+		GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+		} 
+	}
+	
+	IEnumerator ReadyToFall ()
+	{
+		CanFall = false;
+		yield return new WaitForSeconds(TimeBeforeFall);
+		
+		CanFall = true;
+	}
+}
+
