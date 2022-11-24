dc2b9b0fcd745ab1d2f793b566eda7f75f94dd1d
Paulinho <paulo.tarso@escolar.ifrn.edu.br>
2022-11-24 00:10:39 -0300

    Falling Platforms ADDED

--- Assets/Scripts/Cam.cs
+++ Assets/Scripts/Cam.cs
@@ -4,25 +4,9 @@ using UnityEngine;
 
 public class Cam : MonoBehaviour
 {
-	// private Transform player;
-	// public float smooth;
-
-	// void Start()
-	// {
-	// 	player = GameObject.FindGameObjectWithTag("Player").transform;
-	// }
-	
-	// void FixedUpdate()
-	// {
-	// 	if(player.position.x >= -1.6)
-	// 	{
-	// 		Vector3 following = new Vector3(player.position.x, transform.position.y, transform.position.z);
-	// 		transform.position = Vector3.Lerp(transform.position, following, smooth * Time.deltaTime);
-	// 	}	
-	// }
-
 	public Transform target;
 	public float smoothSpeed;
+	public float altura_camera = 2f;
 
 	void Start()
 	{
@@ -31,7 +15,7 @@ public class Cam : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		Vector3 startPosition = new Vector3(target.position.x, (target.position.y + 2f), -1f);
+		Vector3 startPosition = new Vector3(target.position.x, (target.position.y + altura_camera), -1f);
 		Vector3 smoothPosition = Vector3.Lerp(transform.position, startPosition, smoothSpeed);
 		transform.position = smoothPosition;
 	}
---
+++ Assets/Scripts/FallPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlatform : MonoBehaviour {
+
+	Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player")
+		{
+			Invoke("DropPlatform", 0.5f);
+			Destroy(gameObject, 2f);
+		}
+	}
+
+	void DropPlatform()
+	{
+		rb.isKinematic = false;
+	}
+}
